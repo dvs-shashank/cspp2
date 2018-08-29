ceeaf45f603871c7b77aec518d4e4d783fa55b9e
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-29 12:20:47 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m3/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -3,42 +3,42 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * { var_description }.
-	 */
-	public static final int SEVEN = 7;
-	/**
-	 * { var_description }.
-	 */
-	public static final int TEN = 10;
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * { var_description }.
+     */
+    public static final int SEVEN = 7;
+    /**
+     * { var_description }.
+     */
+    public static final int TEN = 10;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * Counts the number of 7.
-	 *
-	 * @param      n     { number given from user }.
-	 *
-	 * @return     Number of 7.
-	 */
-	public static int count7(final int n) {
-		int count = 0;
-		//int temp = 0;
-		for (int i = 1; i <= n; i++) {
-			int temp = i;
-			while (temp > 1) {
-				int r = temp % TEN;
-				if (r == SEVEN) {
-					count += 1;
-				}
-				temp = temp / TEN;
-			}
-		}
-		return count;
-	}
+    }
+    /**
+     * Counts the number of 7.
+     *
+     * @param      n     { number given from user }.
+     *
+     * @return     Number of 7.
+     */
+    public static int count7(final int n) {
+        int count = 0;
+        //int temp = 0;
+        for (int i = 1; i <= n; i++) {
+            int temp = i;
+            while (temp > 1) {
+                int r = temp % TEN;
+                if (r == SEVEN) {
+                    count += 1;
+                }
+                temp = temp / TEN;
+            }
+        }
+        return count;
+    }
 
 /* Fill the main function to print the number of 7's between 1 to n*/
 /**
@@ -50,6 +50,14 @@ public final class Solution {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int res = count7(n);
-       	System.out.println(res);
+        System.out.println(res);
     }
 }
+
+
+
+
+
+
+
+
