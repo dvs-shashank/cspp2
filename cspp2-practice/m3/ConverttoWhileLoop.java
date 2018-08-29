658f2ffd344531ac7ead2c813d4c0284ba40a01b
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-29 10:34:34 +0530

    ConverttoWhileLoop

---
+++ cspp2-practice/m3/ConverttoWhileLoop.java
@@ -0,0 +1,27 @@
+/**
+ * Class for convertto while loop.
+ */
+public final class ConverttoWhileLoop {
+    /**
+     * Constructs the object.
+     */
+    private ConverttoWhileLoop() {
+
+    }
+    /**
+     * { main method }.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(String[] args) {
+        int i = 2;
+        /**
+         * while loop to print.
+         */
+        while(i <= 10) {
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("GoodBye!"); 
+    }
+}
