94da90ff0a4c637576f51e587e66fac8bc9019c1
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-30 14:20:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m3/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m3/m4/Assignment-1/Solution.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
 import java.util.Arrays;
-public final class Solution
-{
+/**
+ * Class for solution.
+ */
+public final class Solution {
 /**
  * Constructs the object.
  */
-	private Solution() {
+    private Solution() {
 
-	}
-	/**
-	 * { Main method }.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(String[] args)
-	{
-		//System.out.println("enter size of array");
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		int arr[] = new int[size];
-		for (int i = 0;i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		System.out.println(arr[size - 1]);
-	}
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        //System.out.println("enter size of array");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0;i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[size - 1]);
+    }
 }
+
+
