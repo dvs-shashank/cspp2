0688a1a73bbb157f0aa77f1b9a0d24e3b942c9af
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-30 14:22:18 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m3/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m3/m4/Assignment-1/Solution.java
@@ -11,21 +11,32 @@ public final class Solution {
 
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
-     * @param      args  The arguments
+     * @param      args  The arguments.
      */
     public static void main(final String[] args) {
         //System.out.println("enter size of array");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] arr = new int[size];
-        for (int i = 0;i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
         System.out.println(arr[size - 1]);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
