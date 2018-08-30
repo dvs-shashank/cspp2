73157a8b619e3fa09f9bcb5c93c7fbdfb15aff32
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-30 14:08:33 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/m4/Assignment-1/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public final class Solution
+{
+/**
+ * Constructs the object.
+ */
+	private Solution() {
+
+	}
+	/**
+	 * { Main method }.
+	 *
+	 * @param      args  The arguments.
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("enter size of array");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0;i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[size - 1]);
+	}
+}
