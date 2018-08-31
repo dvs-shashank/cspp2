7c3d52480ab1e91924001bd7c50c0939575a073a
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-31 16:35:13 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -5,37 +5,42 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { reversing the string. }.
-	 *
-	 * @param      str   The string.
-	 *
-	 * @return     { reversed string }.
-	 */
-	public static String reverseString(final String str) {
-		String rev = "";
-		for (int i = str.length() - 1; i >= 0; i--) {
-			rev += str.charAt(i);
-		}
-		return rev;
-	}
-	/**
-	 * { main method }.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
+    }
+    /**
+     * { reversing the string. }.
+     *
+     * @param      str   The string.
+     *
+     * @return     { reversed string }.
+     */
+    public static String reverseString(final String str) {
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        return rev;
+    }
+    /**
+     * { main method }.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);  
+        System.out.println(reverse);
+    }
 }
+
+
+
+
+
+
+
