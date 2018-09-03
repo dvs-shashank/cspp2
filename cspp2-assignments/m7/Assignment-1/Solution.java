b14d79213f9f9a53331f2550f2c8ce59b8481a45
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-03 20:13:20 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -37,11 +37,10 @@ public class Solution {
 	 * 
 	 * @param      args  args[].
 	 */
-	public static void main(final String args[])
-    {
+	public static void main(final String[] args) {
     	Scanner s = new Scanner(System.in);
     	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
+    	InputValidator i = new InputValidator(input);
     	System.out.println(i.validateData());
 
     }
