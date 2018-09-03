4fe467f053d251031ca1cb66ba13fa0596380d56
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-03 20:19:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -3,43 +3,46 @@ import java.util.Scanner;
  * Class for input validator.
  */
 class InputValidator {
-	/**
-	 * input object variable.
-	 */
-	private String input;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      str   The string.
-	 */
-	InputValidator(final String str) {
-		this.input = str;
-	}
-	/**
-	 * Function which is used for validating.
-	 *
-	 * @return     { boolean type }.
-	 */
-	public boolean validateData() {
-		final int len = this.input.length();
-		if (len >= 6) {
-			return true;
-		}
-		return false;
-	}
+    public static final int SIX = 6;
+    /**
+     * input object variable.
+     */
+    private String input;
+    /**
+     * Constructs the object.
+     *
+     * @param      str   The string.
+     */
+    InputValidator(final String str) {
+        this.input = str;
+    }
+    /**
+     * Function which is used for validating.
+     *
+     * @return     { boolean type }.
+     */
+    public boolean validateData() {
+        final int len = this.input.length();
+        if (len >= SIX) {
+            return true;
+        }
+        return false;
+    }
 }
 /**
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * main method.
-	 * @param      args  args[].
-	 */
-	public static void main(final String[] args) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);
-    	System.out.println(i.validateData());
+    /**
+     * main method.
+     * @param      args  args[].
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
     }
 }
+
+
