ae9cbff523442421e5131ad69c4d25511c50d3b0
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-04 16:00:23 +0530

    "check_style_score -> 1 of 2 passed. style score: 1/1 "

--- cspp2-assignments/m8/CodecampList/List.java
+++ cspp2-assignments/m8/CodecampList/List.java
@@ -313,7 +313,7 @@ public class List {
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
                 break;
                 default:
-                System.out.println("");
+                break;
             }
         }
     }
