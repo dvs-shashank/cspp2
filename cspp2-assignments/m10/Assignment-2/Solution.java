e29a591ff2a24a5298024e18b21d33e37194223d
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-06 14:54:53 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/Solution.java
+++ cspp2-assignments/m10/Assignment-2/Solution.java
@@ -12,7 +12,7 @@ public final class Solution {
 	public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface) sl ;
+        StringListInterface l = (StringListInterface) sl;
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -63,3 +63,8 @@ public final class Solution {
         }
 	}
 }
+
+
+
+
+
