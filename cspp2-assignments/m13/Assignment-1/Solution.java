6815ac5bec68e111d208d4973fe6c69d40c90b23
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-08 12:53:33 +0530

    "check_style_score -> 3 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m13/Assignment-1/Solution.java
+++ cspp2-assignments/m13/Assignment-1/Solution.java
@@ -139,7 +139,7 @@ class Set {
      * @return     { int[][] cartesian product }.
      */
     public int[][] cartesianProduct(final Set setObj) {
-        int[][] temp = new int[2][setObj.size];
+        int[][] temp = new int[setObj.size()][2];
         return temp;
     }
 }
