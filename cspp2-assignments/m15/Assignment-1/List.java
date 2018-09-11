03ddfddae17da3f86e95c4a363f90073535dc395
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-09-11 18:15:10 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/List.java
+++ cspp2-assignments/m15/Assignment-1/List.java
@@ -16,88 +16,13 @@ public class List {
 	 * size.
 	 */
 	private int size;
-	//Implement all the methods mentioned to build a ListADT
-
-	/*
-	 * The goal for the list is to store items.
-	 * How are we going to store the items in the list?
-	 * An array would be good. Right?
-	 * So, assume we are only going to have ints in the list
-	 * We need to create an array of ints to store the items
-	 * added to the list.
-	 *
-	 * Create a variable of the type int[].
-	 * Use the private access specifier
-	 * Why private access specifier and why not public?
-	 * Well, we don't want the array to be manipulated by
-	 * methods that are outside the List class.
-	 * If you allow methods outside the List class to manipulate
-	 * the array then there is a possibility of having a corrupted
-	 * list i.e., a list with incorrect items.
-	 * This is not desirable and so having private access specifer
-	 * will protect the array such corruption.
-	 * This is a hard concept to understand. Discuss with your mentor.
-	 *
-	*/
-
-	// declare a private int[]
-	// don't create the array yet using new
-	// that's the job of the List constructor
-
-	/*
-	 * What are the other class variables needed for creating a list?
-	 * How about keeping track of the size of the list?
-	 * If you add 2 items to the list then the size should be 2.
-	 * Let's think about the size of the list by comparing it to the
-	 * length of the array. Do they mean the same?
-	 * No, Array length signifies the maximum number of items
-	 * you can store in the list. Whereas, the size of the list
-	 * denotes the number of items in the list. Makes sense?
-	 * Here is an example:
-	 * array = [1,2,3,0,0,0,0,0,0,0]
-	 * The length of the array is 10 and size is 3.
-	 * This means you can store 10 items in the list and
-	 * currently it has 3 items.
-	 * So, to keep track of the size we need a variable called size
-	 * Again, we use private as we don't want that size variable
-	 * to be accessed by the methods that are outside of the List class.
-	 *
-	 */
-
-	// declare a private int size
-	// again, don't initialize it here
-	// variable initialization should be done in the constructor
-
-	/*
-	 * The purpose of the constructor is to initialize the 
-	 * class variables with
-	 * some default values.
-	 */
-
 	/**
 	 * Constructs the object.
 	 */
 	public List() {
-
-		// what are the two variables to be initialized here? think about the
-		// private variables described above. What should be the default values?
-		// In the case of the list, it should be empty but it should be
-		// initialized with an array size like 10
-		// Think about the initial value for size.
-		// How many items do we have in the list when you create it?
-		// An empty list has how many items?
-		// That is the initial value to use for size.
 		size = 0;
 		list = new int[SIZE];
 	}
-	/*
-	 * The add method does what the name suggests. Add an int item to the list.
-	 * The assumption is to store the item at the end of the list What is the
-	 * end of the list? Is it the same as the end of the array? Think about how
-	 * you can use the size variable to add item to the list.
-	 *
-	 * The method returns void (nothing)
-	 */
 
 	/**
 	 * adds items to the list.
@@ -120,20 +45,7 @@ public class List {
 		}
 		return count;
 	}
-	/*
-	 * The remove method does what the name suggests. Removes an int item,
-	 * specified by the index argument, from the list It also does an additional
-	 * step. Think about what happens when an item is removed from the middle of
-	 * the list It creates a hole in the list, right? This would mean, all the
-	 * items that are to the right side of the removed item should be moved to
-	 * the left by one position. Here is an example: array =
-	 * [1,2,3,0,0,0,0,0,0,0] remove(1) would remove the item 2 which is at index
-	 * position 1. But how do you remove the item from an array? Well, the way
-	 * to remove it is to move all the items, that are to the right of the
-	 * removed item, to the left So, the new array looks like this. array =
-	 * [1,3,0,0,0,0,0,0,0,0] The method returns void (nothing)
-	 *
-	 */
+
 	/**
 	 * removes ana element at an index position.
 	 *
@@ -150,21 +62,13 @@ public class List {
 			System.out.println("Invalid Position Exception");
 		}
 	}
-	/*
-	 * The remove method does what the name suggests. Removes an int item,
-	 * specified by the index argument, from the list It also does an additional
-	 * step. Think about what happens when an item is removed from the middle of
-	 * the list It creates a hole in the list, right? This would mean, all the
-	 * items that are to the right side of the removed item should be moved to
-	 * the left by one position. Here is an example: array =
-	 * [1,2,3,0,0,0,0,0,0,0] remove(1) would remove the item 2 which is at index
-	 * position 1. But how do you remove the item from an array? Well, the way
-	 * to remove it is to move all the items, that are to the right of the
-	 * removed item, to the left So, the new array looks like this. array =
-	 * [1,3,0,0,0,0,0,0,0,0] The method returns void (nothing)
+	/**
+	 * counts the frequency.
 	 *
+	 * @param      item  The item
+	 *
+	 * @return     {  frequency }
 	 */
-
 	public int count(int item) {
 		int count =0;
 		for (int i = 0; i < size; i++) {
@@ -185,20 +89,6 @@ public class List {
 	public int get(final int index) {
 		return list[index];
 	}
-	/*
-	 * What happens when you print an object using println? Java provides a
-	 * method named toString that is internally invoked when an object variable
-	 * is used in println. For example: List l = new List();
-	 * System.out.println(l); This statement is a shortcut for
-	 * System.out.println(l.toString());
-	 *
-	 * So, implement the toString method to display the items in the list in the
-	 * square brackets notation. i.e., if the list has numbers 1, 2, 3 return
-	 * the string [1,2,3] Caution: The array may be having other elements
-	 * Example: [1,2,3,0,0,0,0,0,0,0] toString should only return the items in
-	 * the list and not all the elements of the array.
-	 */
-
 	/**
 	 * Returns a string representation of the object.
 	 *
@@ -215,20 +105,6 @@ public class List {
 		str += "]";
 		return str;
 	}
-
-	/*
-	 * What happens when you print an object using println? Java provides a
-	 * method named toString that is internally invoked when an object variable
-	 * is used in println. For example: List l = new List();
-	 * System.out.println(l); This statement is a shortcut for
-	 * System.out.println(l.toString());
-	 *
-	 * So, implement the toString method to display the items in the list in the
-	 * square brackets notation. i.e., if the list has numbers 1, 2, 3 return
-	 * the string [1,2,3] Caution: The array may be having other elements
-	 * Example: [1,2,3,0,0,0,0,0,0,0] toString should only return the items in
-	 * the list and not all the elements of the array.
-	 */
 	/**
 	 * { checks if an element is present or not }.
 	 *
@@ -239,11 +115,6 @@ public class List {
 	public boolean contains(final int item) {
 		return indexOf(item) > 0;
 	}
-	/*
-	 * Returns the index of the first occurrence of the specified element in
-	 * this list, or -1 if this list does not contain the element.
-	 */
-
 	/**
 	 * Searches for the first match.
 	 *
@@ -259,10 +130,6 @@ public class List {
 		}
 		return -1;
 	}
-	/*
-	Inserts all the elements of specified int array to the end of list
-	*/
-
 	/**
 	 * Adds all elements to list.
 	 *
@@ -273,11 +140,6 @@ public class List {
 			add(newArray[i]);
 		}
 	}
-	/*
-	 Removes all of its elements that are contained in the specified int
-	 array.
-	*/
-
 	/**
 	 * Removes all elements from the list.
 	 *
@@ -293,14 +155,6 @@ public class List {
 			}
 		}
 	}
-	/*
-	Returns a list object containing elements, including startIndex and
-	excluding endIndex. The first parameter indicates the startIndex and the
-	second parameter indicates the endIndex. Returns null and print
-	"Index Out of Bounds Exception" if any of values start and end are negative
-	and also if start is greater than end.
-	*/
-
 	/**
 	 * { sublist function implementation. }.
 	 *
@@ -331,10 +185,7 @@ public class List {
 		}
 		return null;
 	}
-	/*
-	Returns a boolean indicating whether the parameter i.e a List object is
-	exactly matching with the given list or not.
-	*/
+
 
 	/**
 	 * { method for checking equality of two lists }.
@@ -356,11 +207,7 @@ public class List {
 		}
 		return false;
 	}
-	/*
-	* Removes all the elements from list
-	* Think about this case and make the method
-	* the simpler.
-	*/
+
 	/**
 	 * clears the whole list.
 	 */
